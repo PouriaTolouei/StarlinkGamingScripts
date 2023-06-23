672259bc9810f4f301dda0b114010efe7c1c04ea
PouriaTolouei <pouria81.t@gmail.com>
2023-06-23 13:50:57 -0700

    Bug fixes

--- RunTests.py
+++ RunTests.py
@@ -1,24 +1,37 @@
-import subprocess
-import paramiko
+import paramiko, threading, time
 
-# Create an SSH client instance 
-client = paramiko.SSHClient() 
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
- 
-# Connect to the server 
-client.connect("100.64.165.41", username="pouriatolouei", password="pt230213") 
+HOST = "100.99.232.31"
+GUEST = "100.64.165.41"
 
-for i in range (1, 2):
-    print("################## Round " + str(i) +  " ##################")
+
+def runTest(ip: str):
+    # Create an SSH client instance 
+    client = paramiko.SSHClient() 
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    # Connect to the server 
+    client.connect(ip, username="pouriatolouei", password="pt230213") 
+
     # Execute commands on the server 
     stdin, stdout, stderr = client.exec_command("DISPLAY=:0 nohup python3 /home/pouriatolouei/Documents/StarLinkGamingScripts/GFNAutomationLinux.py") 
-    
+
+    print(ip)
     # Print the errors 
     errors = stderr.readlines()
     for error in errors:
         print(error.strip())
-    
+
     print(stdout.readlines())
 
-# Close the SSH connection 
-client.close() 
+
+for i in range (1, 11):
+    print("################## Round " + str(i) +  " ##################")
+
+    host = threading.Thread(target=runTest, args=[HOST])
+    guest = threading.Thread(target=runTest, args=[GUEST])
+
+    host.start()
+    time.sleep(5)
+    guest.start()
+    
+    host.join()
+    guest.join()
