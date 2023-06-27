e6e93db51facb1855747eb16395d31bc37f162c8
pouriatolouei <pouria81.t@gmail.com>
2023-06-27 12:00:59 -0700

    Add boxplot for percentage of packet loss

--- AnalyzeMetrics.py
+++ AnalyzeMetrics.py
@@ -1,5 +1,6 @@
 import matplotlib, csv, os, statistics
 import matplotlib.pyplot as plt
+import scapy
 
 NONE = -1
 AVERAGE = 0
@@ -12,10 +13,12 @@ INPUTLATENCY = 3
 pings = []
 packetLosses = []
 inputLatencies = []
+packetLossPercent = []
 
 pingStats = [[], []]
 packetLossStats = [[], []]
 inputLatencyStats = [[], []]
+
 
 def storeStats(stats, metrics):
     stats[0].append(statistics.mean(metrics))
@@ -56,19 +59,35 @@ while exists:
             storeStats(packetLossStats, packetLosses)
             storeStats(inputLatencyStats, inputLatencies)
         
+        numPackets = 0
+
+        with open('Capture' + str(i) + '.pcap') as pcap_file:
+            for packet in scapy.PcapReader(pcap_file):
+                numPackets += 1
+        
+        packetLossPercent.append((packetLosses[i - 1] // numPackets) * 100)
+        
         os.chdir('..')
         i += 1
         
     except FileNotFoundError:
         exists = False
 
+
+
+
+
 graphBoxPlot(pings, NONE, "Ping (ms)", "Pings", 0, 160, 10)
-graphBoxPlot(pingStats, AVERAGE, "Ping (ms)", "PingAverages", 0, 160, 10)
+# graphBoxPlot(pingStats, AVERAGE, "Ping (ms)", "PingAverages", 0, 160, 10)
 graphBoxPlot(pingStats, DEVIATION, "Ping (ms)", "PingDeviations", 0, 110, 10)
 
 graphBoxPlot(inputLatencies, NONE, "Input Latency (ms)", "InputLatencies", 0, 260, 10)
-graphBoxPlot(inputLatencyStats, AVERAGE, "Input Latency (ms)", "InputLatencyAverages", 0, 260, 10)
+# graphBoxPlot(inputLatencyStats, AVERAGE, "Input Latency (ms)", "InputLatencyAverages", 0, 260, 10)
 graphBoxPlot(inputLatencyStats, DEVIATION, "Input Latency (ms)", "InputLatencyDeviations", 0, 210, 10)
+
+graphBoxPlot(packetLossPercent, NONE, "Packet Loss (%)", "PacketLossPercent", 0, 110, 10)
+
+
 
 # graphBoxPlot(inputLatencyStats, AVERAGE, "Packet Loss", "PacketLossAverages", -1, 11, 1)
 # graphBoxPlot(inputLatencyStats, DEVIATION, "Packet Loss", "PacketLossDeviations", 0, 110, 10)
