aebf20ac337738c0393331c4e893e6da114939f1
pouriatolouei <pouria81.t@gmail.com>
2023-06-27 10:18:40 -0700

    Fix both box plots being input latencies

--- AnalyzeMetrics.py
+++ AnalyzeMetrics.py
@@ -60,8 +60,8 @@ while exists:
     except FileNotFoundError:
         exists = False
 
-graphBoxPlot(inputLatencyStats, AVERAGE, "Ping (ms)", "PingAverages", 20, 130, 10)
-graphBoxPlot(inputLatencyStats, DEVIATION, "Ping (ms)", "PingDeviations", 0, 110, 10)
+graphBoxPlot(pingStats, AVERAGE, "Ping (ms)", "PingAverages", 20, 130, 10)
+graphBoxPlot(pingStats, DEVIATION, "Ping (ms)", "PingDeviations", 0, 110, 10)
 
 graphBoxPlot(inputLatencyStats, AVERAGE, "Input Latency (ms)", "InputLatencyAverages", 40, 210, 10)
 graphBoxPlot(inputLatencyStats, DEVIATION, "Input Latency (ms)", "InputLatencyDeviations", 0, 210, 10)
