ed6ab3dc2a0f3628a4233f3437e1b821e527cbac
Pouria Tolouei <pouriatolouei@Pourias-Mac-mini.local>
2023-08-22 12:30:32 -0700

    Rearrange files

---
+++ DataAnalysis/SVic_SVan/AnalyzeMetricsCombined.py
@@ -0,0 +1,211 @@
+import csv, os, statistics
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+
+#------------------------- Variables and Constants  ------------------------------ 
+# Boxplot stat types
+NONE = -1
+AVERAGE = 0
+DEVIATION = 1
+
+# Metrics array positions
+SEC = 0
+TIME = 1
+PING = 2
+PACKETLOSS = 3
+INPUTLATENCY = 4
+FPS = 4
+USEDBAND = 5
+RESOLUTION = 6
+AVAILBAND = 7
+
+# Categories 
+resolutionCategories = ['480 x 360 (16:9)', '960 x 540 (16:9)', '1280 x 720 (16:9)', '1366 x 768 (16:9)', '1600 x 900 (16:9)', '1920 x 1080 (16:9)']
+fpsCategories = list(range(0, 91))
+seconds = list(range(0, 60))
+
+#-------------------------------- Methods ---------------------------------------- 
+
+# Graphs boxplot for the metric passed in
+def graphBoxPlot(metrics1, metrics2, yLabel, fileName, minm, maxm, step, width, height):
+    metrics = {'Victoria Starlink 1': metrics1, 'Victoria Starlink 2': metrics2}
+    plt.figure(figsize =(width, height))
+    plt.ylim(minm, maxm)
+    plt.yticks(range(minm, maxm, step))
+    plt.boxplot(metrics.values(), labels=metrics.keys())
+    plt.ylabel(yLabel)
+    plt.savefig(fileName + ".jpg")
+    plt.clf()
+    plt.close()
+
+# Graphs boxplot for the metric passed in
+def graphBoxPlotDecimal(metrics1, metrics2, yLabel, fileName, minm, maxm, step, width, height):
+    metrics = {'Victoria Starlink 1': metrics1, 'Victoria Starlink 2': metrics2}
+    plt.figure(figsize =(width, height))
+    plt.ylim(minm, maxm)
+    plt.yticks(np.arange(minm, maxm, step))
+    plt.boxplot(metrics.values(), labels=metrics.keys())
+    plt.ylabel(yLabel)
+    plt.savefig(fileName + ".jpg")
+    plt.clf()
+    plt.close()
+
+# Graphs CDF for the metric passed in
+def graphDistr(metrics1, metrics2, xLabel, fileName, minm, maxm, step, width, height):
+    plt.figure(figsize =(width, height))
+    plt.xlim(minm, maxm)
+    plt.xticks(range(minm, maxm, step))
+    plt.ylim(0, 1.1)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    x1 = np.sort(metrics1)
+    y1 = np.arange(len(metrics1) + 1) / float(len(metrics1))
+    x1 = np.insert(x1, 0, x1[0] - 1)
+    set1 = {}
+    for i in range(len(x1)):
+        set1[x1[i]] = y1[i]
+
+    x2 = np.sort(metrics2)
+    y2 = np.arange(len(metrics2) + 1) / float(len(metrics2))
+    x2 = np.insert(x2, 0, x2[0] - 1)
+    set2 = {}
+    for i in range(len(x2)):
+        set2[x2[i]] = y2[i]
+
+    plt.plot(set1.keys(), set1.values(), marker='', label='Victoria Starlink 1 CDF', color='red')
+    plt.plot(set2.keys(), set2.values(), marker='', label='Victoria Starlink 2 CDF', color='blue')
+    plt.legend(loc="upper left")
+    plt.xlabel(xLabel)
+    plt.ylabel('Likelihood of occurrence')
+    plt.savefig(fileName + "Distr.jpg")
+    plt.margins(0)
+    plt.clf()
+    plt.close()
+
+
+# Reads all the raw data from the CSV files in round folders and organizes them into arrays
+def extractData(latenciesTime, metricsTime, inputLatencies, pings, packetLosses, usedBandWidths, availableBandwidths, frames, resolutions, totalPacketLosses):
+    roundNum = 1
+
+    # Keeps extracting data while the round folders exist
+    while roundNum <= 20 :
+        try:
+            # Enters each round folder and prints it to confirm
+            os.chdir(str(roundNum))
+            print(os.getcwd())
+
+            roundPacketLosses = []
+
+            # Reads the Latencies(roundNum).csv file
+            with open('Latencies' + str(roundNum) + '.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count > 0:
+                        latenciesTime.append(datetime.strptime(row[TIME], '%Y-%m-%d %H:%M:%S.%f'))
+                        inputLatencies.append(float(row[INPUTLATENCY]))
+                    
+                    line_count += 1
+
+            # Reads the Metrics(roundNum).csv file
+            with open('Metrics' + str(roundNum) + '.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count > 0:
+                        metricsTime.append(datetime.strptime(row[TIME][0:19], '%Y-%m-%d %H:%M:%S'))
+                        pings.append(int(row[PING]))
+                        packetLosses.append(int(row[PACKETLOSS]))
+                        roundPacketLosses.append(int(row[PACKETLOSS]))
+                        usedBandWidths.append(int(row[USEDBAND]))
+                        availableBandwidths.append(int(row[AVAILBAND]))
+                        frames.append(int(row[FPS]))
+                        resolutions.append(row[RESOLUTION])
+                    
+                    line_count += 1
+            
+            totalPacketLosses.append(sum(roundPacketLosses))
+
+            # Exits round folder and moves on to the next
+            os.chdir('..')
+            roundNum += 1
+        
+        except FileNotFoundError:
+            roundNum += 1
+            pass
+        
+
+#-------------------------------- Execution ---------------------------------------- 
+# Array of metrics
+latenciesTime1 = [] # Times from the metrics files
+metricsTime1 = [] # Times from the latencies files
+pings1 = []
+inputLatencies1 = []
+packetLosses1 = []
+usedBandWidths1 = []
+frames1 = []
+resolutions1 = []
+availableBandwidths1 = []
+totalPacketLosses1 = []
+packetLossRatios1 = []
+
+latenciesTime2 = [] # Times from the metrics files
+metricsTime2 = [] # Times from the latencies files
+pings2 = []
+inputLatencies2 = []
+packetLosses2 = []
+usedBandWidths2 = []
+frames2 = []
+resolutions2 = []
+availableBandwidths2 = []
+totalPacketLosses2 = []
+packetLossRatios2 = []
+
+testNum = 1 
+folderNames = ['Vic (Panbee)', 'Vic (Pantrig)']
+data = {folderNames[0]: [latenciesTime1, metricsTime1, inputLatencies1, pings1, packetLosses1, usedBandWidths1, availableBandwidths1, frames1, resolutions1, totalPacketLosses1], folderNames[1]: [latenciesTime2, metricsTime2, inputLatencies2, pings2, packetLosses2, usedBandWidths2, availableBandwidths2, frames2, resolutions2, totalPacketLosses2]}
+
+for folderName in folderNames:
+    os.chdir(folderName)
+    # Keeps analyzing while the test folders exist
+    for i in range(1,5):
+    
+        # Enters each test folder for analysis
+        os.chdir("Test" + str(i))
+        extractData(data[folderName][0], data[folderName][1], data[folderName][2], data[folderName][3], data[folderName][4], data[folderName][5], data[folderName][6], data[folderName][7], data[folderName][8], data[folderName][9])
+
+        # Exits the test folder and moves on to the next
+        os.chdir('..')
+        
+    os.chdir('..')
+
+try:
+    os.mkdir("Analysis")
+except:
+    pass
+
+os.chdir("Analysis")
+
+for totalPacketloss1 in totalPacketLosses1:
+    packetLossRatios1.append((totalPacketloss1 / 250000)*100)
+
+for totalPacketloss2 in totalPacketLosses2:
+    packetLossRatios2.append((totalPacketloss2 / 250000)*100)
+
+
+# Graph generation
+graphBoxPlot(pings1, pings2, "Ping (ms)", "Pings", 0, 260, 10, 6, 8)
+graphDistr(pings1, pings2, "Ping (ms)", "Pings", 0, 260, 10, 11, 9)
+
+graphBoxPlot(inputLatencies1, inputLatencies2, "Input Latency (ms)", "InputLatencies", 0, 260, 10, 6, 8)
+graphDistr(inputLatencies1, inputLatencies2, "Input Latency (ms)", "InputLatencies", 0, 260, 10, 11, 9)
+
+graphBoxPlot(usedBandWidths1, usedBandWidths2, "Used BandWidth (Mbps)", "UsedBandwidths", 0, 52, 2, 6, 8)
+graphDistr(usedBandWidths1, usedBandWidths2, "Used BandWidth (Mbps)", "UsedBandwidths", 0, 52, 2, 11, 9)
+
+graphBoxPlot(availableBandwidths1, availableBandwidths2, "Available BandWidth (Mbps)", "AvailableBandwidths", 0, 160, 10, 6, 8)
+graphDistr(availableBandwidths1, availableBandwidths2, "Available BandWidth (Mbps)", "AvailableBandwidths", 0, 160, 10, 11, 9)
+
+graphBoxPlot(totalPacketLosses1, totalPacketLosses2, "Total Packet Loss", "TotalPacketLoss", 0, 3600, 200, 6, 8)
+graphDistr(totalPacketLosses1, totalPacketLosses2, "Total Packet Loss", "TotalPacketLoss", 0, 3600, 200, 11, 9)
+graphBoxPlotDecimal(packetLossRatios1, packetLossRatios2, "Total Packet Loss (%)", "PacketLossRatios", 0, 1.1, 0.1, 6, 8)
---
+++ DataAnalysis/SVic_Telus/Telus/AnalyzeMetricsIndividual.py
@@ -0,0 +1,308 @@
+import csv, os, statistics
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+
+#------------------------- Variables and Constants  ------------------------------ 
+# Boxplot stat types
+NONE = -1
+AVERAGE = 0
+DEVIATION = 1
+
+# Metrics array positions
+SEC = 0
+TIME = 1
+PING = 2
+PACKETLOSS = 3
+INPUTLATENCY = 4
+FPS = 4
+USEDBAND = 5
+RESOLUTION = 6
+AVAILBAND = 7
+
+# Categories 
+resolutionCategories = ['480 x 360 (16:9)', '960 x 540 (16:9)', '1280 x 720 (16:9)', '1366 x 768 (16:9)', '1600 x 900 (16:9)', '1920 x 1080 (16:9)']
+fpsCategories = list(range(0, 91))
+seconds = list(range(0, 60))
+
+#-------------------------------- Methods ---------------------------------------- 
+
+# For each metrics passed in, it calculates and stores the mean and standard deviation
+def storeStats(stats, metrics):
+    stats[0].append(statistics.mean(metrics))
+    stats[1].append(statistics.stdev(metrics))
+
+# Organize and store total packet losses, average pings, and average input latencies by seconds in a minute
+def organizeDataBySecond():
+    for time in metricsTime:
+        metricsTimeSeconds.append(time.time().second)
+
+    for time in latenciesTime:
+        latenciesTimeSeconds.append(time.time().second)
+
+    for i in range (60):
+        totalPacketLossAtSeconds.append(0)
+        pingsAtSeconds.append([])
+        inputLatenciesAtSeconds.append([])
+        availableBandwidthsAtSeconds.append([])
+        usedBandwidthAtSeconds.append([])
+
+    for i in range(len(metricsTimeSeconds)):
+        totalPacketLossAtSeconds[metricsTimeSeconds[i]] += packetLosses[i]
+        pingsAtSeconds[metricsTimeSeconds[i]].append(pings[i])
+        availableBandwidthsAtSeconds[metricsTimeSeconds[i]].append(availableBandwidths[i])
+        usedBandwidthAtSeconds[metricsTimeSeconds[i]].append(usedBandWidths[i])
+
+    for i in range (len(latenciesTimeSeconds)):
+        inputLatenciesAtSeconds[latenciesTimeSeconds[i]].append(inputLatencies[i])
+
+    for i in range(len(seconds)):
+        averagePingAtSeconds.append(statistics.mean(pingsAtSeconds[i]))
+        averageInputLatencyAtSeconds.append(statistics.mean(inputLatenciesAtSeconds[i]))
+        averageAvailableBandwidthAtSeconds.append(statistics.mean(availableBandwidthsAtSeconds[i]))
+        averageUsedBandwidthAtSeconds.append(statistics.mean(usedBandwidthAtSeconds[i]))
+
+# Graphs boxplot for the metric passed in
+def graphBoxPlot(stats, statType, yLabel, fileName, minm, maxm, step):
+    plt.figure(figsize =(6, 8))
+    plt.ylim(minm, maxm)
+    plt.yticks(range(minm, maxm, step))
+    if statType == NONE:
+        plt.boxplot(stats)
+    else:
+        plt.boxplot(stats[statType])
+    plt.ylabel(yLabel)
+    plt.savefig(fileName + ".jpg")
+    plt.clf()
+    plt.close()
+
+# Graphs CDF for the metric passed in
+def graphDistr(metrics, xLabel, fileName, minm, maxm, step):
+    plt.figure(figsize =(11, 9))
+    plt.xlim(minm, maxm)
+    plt.xticks(range(minm, maxm, step))
+    plt.ylim(0, 1.1)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    x = np.sort(metrics)
+    y = np.arange(len(metrics) + 1) / float(len(metrics))
+    x = np.insert(x, 0, x[0] - 1)
+    set1 = {}
+    for i in range(len(x)):
+        set1[x[i]] = y[i]
+
+    plt.plot(set1.keys(), set1.values(), marker='', label='CDF')
+    plt.legend(loc="upper left")
+    plt.xlabel(xLabel)
+    plt.ylabel('Likelihood of occurrence')
+    plt.savefig(fileName + "Distr.jpg")
+    plt.margins(0)
+    plt.clf()
+    plt.close()
+
+#  Graphs bar graphs forshowing metrics by seconds in a minute
+def graphSecondsBar(metrics, ylabel, fileName, minm, maxm, step): 
+    plt.figure(figsize =(17, 9))
+    plt.xlim(-1, 60)
+    plt.xticks(range(0, 60, 1))
+    plt.ylim(minm, maxm)
+    plt.yticks(range(minm, maxm, step))
+    plt.bar(seconds, metrics)
+    plt.xlabel("Second")
+    plt.ylabel(ylabel)
+    plt.savefig(fileName + "AtSeconds.jpg")
+    plt.clf()
+    plt.close()
+
+# Graphs a bar graph with the categories of the metric passed in 
+def graphBar(metrics, categories, xLabel, fileName):
+    # Counts the categories of the metrics
+    categoriesCount = {}
+    for category in categories:
+        categoriesCount[category] = 0
+    for metric in metrics:
+        categoriesCount[metric] += 1
+    counts = list(categoriesCount.values())
+
+    plt.figure(figsize =(15, 9))
+    plt.bar(categories, counts)
+    plt.xlabel(xLabel)
+    plt.ylabel("Frequency")
+    plt.savefig(fileName + ".jpg")
+    plt.clf()
+    plt.close()
+
+
+# Reads all the raw data from the CSV files in round folders and organizes them into arrays
+def extractData():
+    roundNum = 1
+
+    # Keeps extracting data while the round folders exist
+    while roundNum <= 20 :
+        try:
+            # Enters each round folder and prints it to confirm
+            os.chdir(str(roundNum))
+            print(os.getcwd())
+
+            # Temporary round arrays used for average and standard deviation calculations
+            roundPings = []
+            roundPacketLosses = []
+            roundInputLatencies = []
+            roundUsedBandwidths = []
+            roundAvailableBandwidths = []
+
+            # Reads the Latencies(roundNum).csv file
+            with open('Latencies' + str(roundNum) + '.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count > 0:
+                        latenciesTime.append(datetime.strptime(row[TIME], '%Y-%m-%d %H:%M:%S.%f'))
+                        inputLatencies.append(float(row[INPUTLATENCY]))
+                        roundInputLatencies.append(float(row[INPUTLATENCY]))
+                    
+                    line_count += 1
+
+            # Reads the Metrics(roundNum).csv file
+            with open('Metrics' + str(roundNum) + '.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count > 0:
+                        metricsTime.append(datetime.strptime(row[TIME][0:19], '%Y-%m-%d %H:%M:%S'))
+                        pings.append(int(row[PING]))
+                        roundPings.append(int(row[PING]))
+                        packetLosses.append(int(row[PACKETLOSS]))
+                        roundPacketLosses.append(int(row[PACKETLOSS]))
+                        usedBandWidths.append(int(row[USEDBAND]))
+                        roundUsedBandwidths.append(int(row[USEDBAND]))
+                        availableBandwidths.append(int(row[AVAILBAND]))
+                        roundAvailableBandwidths.append(int(row[AVAILBAND]))
+                        frames.append(int(row[FPS]))
+                        resolutions.append(row[RESOLUTION])
+                    
+                    line_count += 1
+            
+            # Calculates and stores the stats
+            storeStats(pingStats, roundPings)
+            storeStats(inputLatencyStats, roundInputLatencies)
+            storeStats(usedBandwidthStats, roundUsedBandwidths)
+            storeStats(availableBandwidthStats, roundAvailableBandwidths)
+            totalPacketLosses.append(sum(roundPacketLosses))
+
+            # Exits round folder and moves on to the next
+            os.chdir('..')
+            roundNum += 1
+        
+        except FileNotFoundError:
+            roundNum += 1
+            pass
+        
+
+#-------------------------------- Execution ---------------------------------------- 
+exists = True
+testNum = 1 
+groupNum = 1
+
+# Enters the Results folder
+os.chdir("Results")
+
+while exists:
+    # Array of metrics
+    latenciesTime = [] # Times from the metrics files
+    metricsTime = [] # Times from the latencies files
+    pings = []
+    inputLatencies = []
+    packetLosses = []
+    usedBandWidths = []
+    frames = []
+    resolutions = []
+    availableBandwidths = []
+
+    # Array of metric stats
+    pingStats = [[], []]
+    inputLatencyStats = [[], []]
+    usedBandwidthStats = [[], []]
+    availableBandwidthStats = [[], []]
+    totalPacketLosses = []
+
+    # Array of seconds
+    metricsTimeSeconds = []
+    latenciesTimeSeconds = []
+
+    # Array of data oragnized by seconds in a minute
+    totalPacketLossAtSeconds = []
+    pingsAtSeconds = []
+    averagePingAtSeconds = []
+    inputLatenciesAtSeconds = []
+    averageInputLatencyAtSeconds = []
+    availableBandwidthsAtSeconds = []
+    averageAvailableBandwidthAtSeconds = []
+    usedBandwidthAtSeconds = []
+    averageUsedBandwidthAtSeconds = []
+    
+    # Keeps analyzing while the test folders exist
+    for i in range(4):
+        try:
+            # Enters each test folder for analysis
+            os.chdir("Test" + str(testNum))
+            extractData()
+
+            # Exits the test folder and moves on to the next
+            os.chdir('..')
+            testNum += 1
+        
+        except FileNotFoundError:
+            # Stops the analysis as no more test folders exist
+            exists = False
+
+    if exists:
+        organizeDataBySecond()
+        try:
+            os.mkdir("Analysis" + str(groupNum))
+        except:
+            pass
+        
+        os.chdir("Analysis" + str(groupNum))
+
+        # Graph generation
+        graphBoxPlot(pings, NONE, "Ping (ms)", "Pings", 0, 260, 10)
+        # graphBoxPlot(pingStats, AVERAGE, "Average Ping (ms)", "PingAverages", 0, 260, 10)
+        graphBoxPlot(pingStats, DEVIATION, "Ping Standard Deviation (ms)", "PingDeviations", 0, 110, 10)
+        graphDistr(pings, "Ping (ms)", "Pings", 0, 260, 10)
+        graphSecondsBar(averagePingAtSeconds, "Average Ping (ms)", "AveragePing", 0, 85, 5)
+
+        graphBoxPlot(inputLatencies, NONE, "Input Latency (ms)", "InputLatencies", 0, 260, 10)
+        # graphBoxPlot(inputLatencyStats, AVERAGE, "Average Input Latency (ms)", "InputLatencyAverages", 0, 260, 10)
+        graphBoxPlot(inputLatencyStats, DEVIATION, "Input Latency Standard Deviation (ms)", "InputLatencyDeviations", 0, 210, 10)
+        graphDistr(inputLatencies, "Input Latency (ms)", "InputLatencies", 0, 260, 10)
+        graphSecondsBar(averageInputLatencyAtSeconds, "Average Input Latency (ms)", "AverageInputLatency", 0, 190, 10)
+
+        graphBoxPlot(usedBandWidths, NONE, "Used BandWidth (Mbps)", "UsedBandwidths", 0, 52, 2)
+        # graphBoxPlot(usedBandwidthStats, AVERAGE, "Average Used BandWidth (Mbps)", "UsedBandwidthAverages", 0, 52, 2)
+        graphBoxPlot(usedBandwidthStats, DEVIATION, "Used BandWidth Standard Deviation (Mbps)", "UsedBandwidthDeviations", 0, 20, 1)
+        graphDistr(usedBandWidths, "Used BandWidth (Mbps)", "UsedBandwidths", 0, 52, 2)
+        graphSecondsBar(averageUsedBandwidthAtSeconds, "Average Used BandWidth (Mbps)", "AverageUsedBandwidth", 0, 52, 2)
+
+        graphBoxPlot(availableBandwidths, NONE, "Available BandWidth (Mbps)", "AvailableBandwidths", 0, 160, 10)
+        # graphBoxPlot(availableBandwidthStats, AVERAGE, "Average Available BandWidth (Mbps)", "AvailableBandwidthAverages", 0, 105, 5)
+        graphBoxPlot(availableBandwidthStats, DEVIATION, "Used BandWidth Standard Deviation (Mbps)", "AvailableBandwidthDeviations", 0, 50, 2)
+        graphDistr(availableBandwidths, "Available BandWidth (Mbps)", "AvailableBandwidths", 0, 160, 10)
+        graphSecondsBar(averageAvailableBandwidthAtSeconds, "Average Available BandWidth (Mbps)", "AverageAvailableBandwidth", 0, 160, 10)
+
+        graphBoxPlot(totalPacketLosses, NONE, "Total Packet Loss", "TotalPacketLoss", 0, 3600, 200)
+        graphDistr(totalPacketLosses, "Total Packet Loss", "TotalPacketLoss", 0, 3600, 200)
+        graphSecondsBar(totalPacketLossAtSeconds, "Total Packet Loss", "TotalPacketLoss", 0, 3600, 200)
+
+        graphBar(resolutions, resolutionCategories, "Resolution", "Resolutions")
+        graphBar(frames, fpsCategories, "FPS", "FPS")
+
+        statFile = open("Stats.txt", "w+")
+        statFile.write(f"Ping (Mean/Median/SD): {statistics.mean(pings)}/{statistics.median(pings)}/{statistics.stdev(pings)}\n")
+        statFile.write(f"Input Latency (Mean/Median/SD): {statistics.mean(inputLatencies)}/{statistics.median(inputLatencies)}/{statistics.stdev(inputLatencies)}\n")
+        statFile.write(f"Round Total Packetloss (Mean/Median/SD): {statistics.mean(totalPacketLosses)}/{statistics.median(totalPacketLosses)}/{statistics.stdev(totalPacketLosses)}\n")
+        statFile.write(f"Available Bandwidth (Mean/Median/SD): {statistics.mean(availableBandwidths)}/{statistics.median(availableBandwidths)}/{statistics.stdev(availableBandwidths)}\n")
+        statFile.write(f"Used Bandwidth (Mean/Median/SD): {statistics.mean(usedBandWidths)}/{statistics.median(usedBandWidths)}/{statistics.stdev(usedBandWidths)}")
+        statFile.close()
+
+        groupNum += 1
+        os.chdir('..')
