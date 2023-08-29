a206ed047c83f4a3f37cd65957f928c5dd607a59
Pouria Tolouei <pouria81.t@gmail.com>
2023-08-29 16:01:15 -0700

    Make changes to make the script more modular

--- RunTests.py
+++ RunTests.py
@@ -1,16 +1,26 @@
 import paramiko, threading, time, sys
 
 HOST = sys.argv[1]
-GUEST = sys.argv[2]
-ROUNDS = int(sys.argv[3])
+HOST_USERNAME = sys.argv[2]
+HOST_PASSWORD = sys.argv[3]
+HOST_TYPE = sys.argv[4]
 
-# Creates the test folder on a remote system
-def createFolder(ip: str) -> int:
-     # Create an SSH client instance 
+GUEST = sys.argv[5]
+GUEST_USERNAME = sys.argv[6]
+GUEST_PASSWORD = sys.argv[7]
+GUEST_TYPE = sys.argv[8]
+
+ROUNDS = int(sys.argv[9])
+
+CAPTURE_PATH = "/home/pouriatolouei/Documents/StarLinkGamingScripts/"
+
+# Creates the test folder on a remote system and returns the current test iteration on that system
+def createFolder(ip: str, user: str, passcode: str) -> int:
+    # Create an SSH client instance 
     client = paramiko.SSHClient() 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
     # Connect to the server 
-    client.connect(ip, username="username", password="password") 
+    client.connect(ip, username=user, password=passcode) 
 
     # Execute commands on the server 
     testNum = 1
@@ -18,9 +28,9 @@ def createFolder(ip: str) -> int:
 
     # Create a folder with the current current iteraion of test in its name
     while not succesfullyCreated:
-            stdin, stdout, stderr = client.exec_command("mkdir /home/pouriatolouei/Documents/StarLinkGamingScripts/Results/Test" + str(testNum))
+            stdin, stdout, stderr = client.exec_command("mkdir " + CAPTURE_PATH + "Results/Test" + str(testNum))
             error = stderr.readline().strip()
-            if error == "mkdir: cannot create directory ‘/home/pouriatolouei/Documents/StarLinkGamingScripts/Results/Test" + str(testNum) + "’: File exists":
+            if error == "mkdir: cannot create directory ‘" + CAPTURE_PATH + "Results/Test" + str(testNum) + "’: File exists":
                 testNum += 1
             else:
                 succesfullyCreated = True
@@ -28,15 +38,15 @@ def createFolder(ip: str) -> int:
     return testNum
 
 # Runs the test script on a remote system
-def runTest(ip: str, type: str):
+def runTest(ip: str, type: str, user: str, passcode: str):
     # Create an SSH client instance 
     client = paramiko.SSHClient() 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
     # Connect to the server 
-    client.connect(ip, username="pouriatolouei", password="pt230213") 
+    client.connect(ip, username=user, password=passcode) 
 
     # Execute commands on the server 
-    stdin, stdout, stderr = client.exec_command("DISPLAY=:0 nohup python3 /home/pouriatolouei/Documents/StarLinkGamingScripts/GFNAutomationLinux.py " + type) 
+    stdin, stdout, stderr = client.exec_command("DISPLAY=:0 nohup python3 " + CAPTURE_PATH + "GFNAutomationLinux.py " + type) 
 
     print(ip)
     # Print the errors 
@@ -47,23 +57,23 @@ def runTest(ip: str, type: str):
     print(stdout.readlines())
 
 # Moves the collected data into the created folder on a remote system
-def moveFiles(ip: str, testNum: int):
+def moveFiles(ip: str, testNum: int, user: str, passcode: str):
      # Create an SSH client instance 
     client = paramiko.SSHClient() 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
     # Connect to the server 
-    client.connect(ip, username="username", password="password") 
+    client.connect(ip, username=user, password=passcode) 
 
     # Execute commands on the server 
-    stdin, stdout, stderr = client.exec_command("mv /home/pouriatolouei/Documents/StarLinkGamingScripts/Results/temp/* /home/pouriatolouei/Documents/StarLinkGamingScripts/Results/Test" + str(testNum)) 
+    stdin, stdout, stderr = client.exec_command("mv " + CAPTURE_PATH + "Results/temp/* " + CAPTURE_PATH + "Results/Test" + str(testNum)) 
     print(stderr.readlines())
 
 
 # ----------------------------------- Execution -----------------------------------------
 
 # Creates the test folder on each of the remote systems
-testNumHost = createFolder(HOST)
-testNumGuest = createFolder(GUEST)
+testNumHost = createFolder(HOST, HOST_USERNAME, HOST_PASSWORD)
+testNumGuest = createFolder(GUEST, GUEST_USERNAME, GUEST_PASSWORD)
 # Ouputs the created directory for confirmation
 print(testNumHost)
 print(testNumGuest)
@@ -73,8 +83,8 @@ for i in range (0, ROUNDS):
     for j in range (1, 6):
         print("################## Round " + str((5*i)+j) +  " ##################")
 
-        host = threading.Thread(target=runTest, args=[HOST, "host"])
-        guest = threading.Thread(target=runTest, args=[GUEST, "host"])
+        host = threading.Thread(target=runTest, args=[HOST, HOST_TYPE, HOST_USERNAME, HOST_PASSWORD])
+        guest = threading.Thread(target=runTest, args=[GUEST, GUEST_TYPE, GUEST_USERNAME, GUEST_PASSWORD])
 
         host.start()
         guest.start()
@@ -86,5 +96,5 @@ for i in range (0, ROUNDS):
     time.sleep(300)
 
 # Moves the collected data to the create folders on the remote systems
-moveFiles(HOST, testNumHost)
-moveFiles(GUEST, testNumGuest)
+moveFiles(HOST, testNumHost, HOST_USERNAME, HOST_PASSWORD)
+moveFiles(GUEST, testNumGuest, GUEST_USERNAME, GUEST_PASSWORD)
